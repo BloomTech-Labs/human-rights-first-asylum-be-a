fc555341c78c07a46dfaf8eb9bca0b607ba01005
Aveline Wingfield <avelinewingfield@gmail.com>
2020-12-10 14:10:59 -0500

    Some tests.

--- __tests__/routes/cases.test.js
+++ __tests__/routes/cases.test.js
@@ -7,14 +7,21 @@ server.use(express.json());
 
 jest.mock('../../api/cases/caseModel');
 
-jest.mock('../../api/middleware/verifyDataID', () =>
-  jest.fn((req, res, next) => next())
-);
-
 describe('cases router endpoints', () => {
   beforeAll(() => {
     // This is the module/route being tested
     server.use(['/case', '/cases'], caseRouter);
     jest.clearAllMocks();
+  });
+
+  describe('GET /cases', () => {
+    it('should return 200', async () => {
+      Case.findAll.mockResolvedValue([]);
+      const res = await request(server).get('/cases');
+
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBe(0);
+      expect(Case.findAll.mock.calls.length).toBe(1);
+    });
   });
 });
