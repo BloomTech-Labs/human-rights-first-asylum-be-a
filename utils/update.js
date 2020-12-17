c7f33ba731bfb29845e67f3a2bb3d604b2a9f4d6
Aveline Wingfield <avelinewingfield@gmail.com>
2020-12-17 10:51:01 -0500

    All Protected Grounds Updated

--- utils/update.js
+++ utils/update.js
@@ -1,18 +1,19 @@
-/* 
+/*
 
 const CronJob = require('cron').CronJob;
-const cacache = require('cacache')
+const cacache = require('cacache');
 const axios = require('axios');
 const Judge = require('../api/judges/judgeModel');
 const Case = require('../api/cases/caseModel');
+const Protected = require('../api/protected/protectedModel');
 
- TODO uncomment code when DS gets a server running
+//  TODO uncomment code when DS gets a server running
 
- * update automatically updates the server - cron is persistant across server updates
+//  * update automatically updates the server - cron is persistant across server updates
 
- * '0 0 * * *' => Runs Daily at Midnight - PST
- * '0 0 * * sun' => Runs Weekly, Midnight Sunday - PST
- ! If DS changes when their backend updates, change cron appropriately
+//  * '0 0 * * *' => Runs Daily at Midnight - PST
+//  * '0 0 * * sun' => Runs Weekly, Midnight Sunday - PST
+//  ! If DS changes when their backend updates, change cron appropriately
 const update = new CronJob(
   '0 0 * * *',
   function () {
@@ -60,9 +61,31 @@ const update = new CronJob(
             .then((ret_case) => {
               if (!ret_case.length) {
                 Case.add(ref_case)
-                  // * continue
-                  .then()
+                  // * continue else catch
                   .catch((err) => console.log(err.message));
+              }
+              if (ref_case.protected_ground.length > 0) {
+                for (let i = 0; i < ref_case.protected_ground.length; i++) {
+                  Protected.findByTag(ref_case.protected_ground[i])
+                    .then((ret_tag) => {
+                      if (ret_tag.length > 0) {
+                        Protected.createJoin(
+                          ref_case[id],
+                          ret_tag[ground_type]
+                        ).catch((err) => console.log(err.message));
+                      } else {
+                        Protected.add(ref_case.protected_ground[i]).then(
+                          (ret) => {
+                            Protected.createJoin(
+                              ref_case[id],
+                              ret_tag[ground_type]
+                            ).catch((err) => console.log(err.message));
+                          }
+                        );
+                      }
+                    })
+                    .catch((err) => console.log(err.message));
+                }
               }
             })
             .catch((err) => {
@@ -76,19 +99,20 @@ const update = new CronJob(
   'America/Los_Angeles'
 );
 
-* runs at 00:30 daily
-* '0 0 * * sun' => Runs Weekly, 00:30 Sunday - PST
+// * runs at 00:30 daily
+// * '0 0 * * sun' => Runs Weekly, 00:30 Sunday - PST
 
 const clearCache = new CronJob(
   '0 30 * * *',
   function () {
-cacache.rm.all(cachePath).then(() => {
-  console.log('THE APOCALYPSE IS UPON US 😱')
-})
-},
+    cacache.rm.all(cachePath).then(() => {
+      console.log('THE APOCALYPSE IS UPON US 😱');
+    });
+  },
   null,
   true,
-  'America/Los_Angeles' );
+  'America/Los_Angeles'
+);
 
 update.start();
 clearCache.start();
