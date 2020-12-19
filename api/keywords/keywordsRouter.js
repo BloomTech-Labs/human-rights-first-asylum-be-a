8c641e24c9bc4a8c739276aa77a9b12dc7a6087f
Aveline Wingfield <avelinewingfield@gmail.com>
2020-12-18 20:46:27 -0500

    All Swagger Get Req's

--- api/keywords/keywordsRouter.js
+++ api/keywords/keywordsRouter.js
@@ -7,6 +7,47 @@ const router = express.Router();
 
 //routes
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Keywords:
+ *      type: object
+ *      required:
+ *        - keywords
+ *      properties:
+ *        keywords:
+ *          type: array
+ *          description: An array of positive/negative keywords
+ *      example:
+ *        keywords: ['soldier', 'woman', 'violence']
+ *
+ * /keywords:
+ *  get:
+ *    description: Returns two lists of keywords
+ *    summary: Get a list of all keywords
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - keywords
+ *    responses:
+ *      200:
+ *        description: array of keywords
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Keywords'
+ *              example:
+ *                - positive_keywords: ['lady', 'violence', 'aide']
+ *                  negative_keywords: ['male', 'january', 'terrorist']
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      403:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ */
+
 router.get('/', Cache.checkCache, (req, res) => {
   const key = String(req.originalUrl);
   Keywords.findAll()
@@ -18,6 +59,33 @@ router.get('/', Cache.checkCache, (req, res) => {
       res.status(500).json({ message: err.message });
     });
 });
+
+/**
+ * @swagger
+ * /keywords/pos:
+ *  get:
+ *    description: Returns two lists of all positive keywords
+ *    summary: Get a list of all positive keywords
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - keywords
+ *    responses:
+ *      200:
+ *        description: array of keywords
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Keywords'
+ *              example:
+ *                - positive_keywords: ['lady', 'violence', 'aide']
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      403:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ */
 
 router.get('/pos', Cache.checkCache, (req, res) => {
   const key = String(req.originalUrl);
@@ -31,6 +99,32 @@ router.get('/pos', Cache.checkCache, (req, res) => {
     });
 });
 
+/**
+ * @swagger
+ * /keywords/neg:
+ *  get:
+ *    description: Returns two lists of all negative keywords
+ *    summary: Get a list of all negative keywords
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - keywords
+ *    responses:
+ *      200:
+ *        description: array of keywords
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/Keywords'
+ *              example:
+ *                - negative_keywords: ['lady', 'violence', 'aide']
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      403:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ */
 router.get('/neg', Cache.checkCache, (req, res) => {
   const key = String(req.originalUrl);
   Keywords.findAllNeg()
