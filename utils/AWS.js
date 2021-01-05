e3101821f99d77837e2ebf89e4545fb44d982461
Aveline Wingfield <avelinewingfield@gmail.com>
2021-01-05 15:46:36 -0500

    Fixed Minor bugs.

--- utils/AWS.js
+++ utils/AWS.js
@@ -6,63 +6,76 @@ const awsConfig = require('../config/awsConfig');
 const S3 = new AWS.S3(awsConfig);
 const fs = require('fs');
 
-const make_view_params = async (case_id) => {
+const make_params = async (case_id) => {
   const curr_case = await Cases.findById(case_id);
   const params = {
     Key: `pdf/${curr_case.case_url}`,
     Bucket: 'human-rights-first-asylum-analysis-documents',
-    ContentType: 'attachment',
-    ContentDisposition: 'application/pdf',
   };
   return params;
 };
 
+// ! s3Stream should ACTUALLY pipe the file into a cacache temp folder
+// TODO set up the fetch to pipe into a cacache temp folder to serve to client
 const fetch_pdf_view = async (params) => {
-  const pdf_file = S3.getObject(params);
-
-  return pdf_file;
-};
-
-const make_dl_params = async (case_id) => {
-  const curr_case = await Cases.findById(case_id);
-  const params = {
-    Key: `pdf/${curr_case.case_url}`,
-    Bucket: 'human-rights-first-asylum-analysis-documents',
-    ContentType: 'attachment',
-    ContentDisposition: 'application/pdf',
-  };
-  return params;
-};
-
-// TODO include case_id in call
-const fetch_pdf_download = async (case_id) => {
-  var fileStream = fs.createWriteStream(`${case_id}.pdf`);
-  var s3Stream = S3.getObject(make_dl_params(case_url)).createReadStream();
+  const title = params.Key;
+  const fileStream = fs.createWriteStream(`${title}`);
+  const s3Stream = S3.getObject(params).createReadStream();
 
   // Listen for errors returned by the service
   s3Stream.on('error', function (err) {
     // NoSuchKey: The specified key does not exist
-    console.error(err);
+    res.status(404).json({ message: err.message });
   });
 
   s3Stream
     .pipe(fileStream)
     .on('error', function (err) {
       // capture any errors that occur when writing data to the file
-      console.error('File Stream:', err);
+      res.status(500).json({ message: err.message });
     })
     .on('close', function () {
       console.log('Done.');
     })
     .on('finish', function () {
+      res.header('Content-Type', 'application/pdf');
+      res.attachment(`${title}.pdf`);
+      res.status(200).render(`${title}.pdf`);
+    });
+
+  return title;
+};
+
+// TODO include case_id in call
+const fetch_pdf_download = async (case_id) => {
+  var fileStream = fs.createWriteStream(`${case_id}.pdf`);
+  var s3Stream = S3.getObject(make_dl_params(case_id)).createReadStream();
+
+  // Listen for errors returned by the service
+  s3Stream.on('error', function (err) {
+    // NoSuchKey: The specified key does not exist
+    res.status(404).json({ message: err.message });
+  });
+
+  s3Stream
+    .pipe(fileStream)
+    .on('error', function (err) {
+      // capture any errors that occur when writing data to the file
+      res.status(500).json({ message: err.message });
+    })
+    .on('close', function () {
+      console.log('Done.');
+    })
+    .on('finish', function () {
+      res.header('Content-Type', 'application/pdf');
+      res.attachment(`${title}.pdf`);
       res.status(200).download(`${case_id}.pdf`);
     });
   return case_id;
 };
 
 module.exports = {
-  make_dl_params,
-  make_view_params,
+  make_params,
   fetch_pdf_view,
   fetch_pdf_download,
 };
