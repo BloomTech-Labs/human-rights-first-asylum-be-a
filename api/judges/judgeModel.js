222ccb3b734d00761c1e612a13ba9286f7999f50
Aveline Wingfield <avelinewingfield@gmail.com>
2021-01-07 19:09:13 -0500

    Zip File Updated

--- api/judges/judgeModel.js
+++ api/judges/judgeModel.js
@@ -220,12 +220,36 @@ const writeCSV = async (name) => {
 
   // * get country data
   const country_data = await countryData(name);
+  const secondary_data = await secondaryData(name);
+
+  // * create social fields
+  const social_fields = [];
+
+  for (let field in secondary_data[0][0]) {
+    social_fields.push(field);
+  }
+  // * create ground fields
+  const grounds_fields = [];
+  for (let field in secondary_data[0][1]) {
+    social_fields.push(field);
+  }
 
   // * create country fields
   const country_fields = [];
   for (let field in country_data[0]) {
     country_fields.push(field);
   }
+
+  // * create opts
+
+  const grounds_opts = {
+    fields: grounds_fields,
+  };
+
+  const social_opts = {
+    fields: social_fields,
+  };
+
   const country_opts = {
     fields: country_fields,
   };
@@ -245,22 +269,20 @@ const writeCSV = async (name) => {
     const judge_parser = new Parser(judge_opts);
     const country_parser = new Parser(country_opts);
     const case_parser = new Parser(case_opts);
+    const social_parser = new Parser(social_opts);
+    const grounds_parser = new Parser(grounds_opts);
     // * fill fields with data
     const judge_csv = judge_parser.parse(judge_data);
     const country_csv = country_parser.parse(country_data);
     const case_csv = case_parser.parse(case_data);
+    const social_csv = social_parser.parse(secondary_data[0]);
+    const grounds_csv = grounds_parser.parse(grounds_data[1]);
 
     // * return array of three variables with csv data
-    return [judge_csv, country_csv, case_csv];
+    return [judge_csv, country_csv, case_csv, social_csv, grounds_csv];
   } catch (err) {
     return err.message;
   }
-};
-
-const writePDF = async (judge_name) => {
-  /* get full judge data, including countries */
-  /* style pdf to display object data in a pleasing manner */
-  // pdf styler in alternate component
 };
 
 module.exports = {
@@ -270,5 +292,4 @@ module.exports = {
   findFullDataByName,
   update,
   writeCSV,
-  writePDF,
 };
