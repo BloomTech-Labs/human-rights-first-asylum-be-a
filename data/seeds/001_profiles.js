de8a5e21216ff88bbe445d71453e94177db6bae9
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2021-01-26 20:00:21 -0500

    Seed profiles match Lambda-issued Okta credentials

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,5 +1,8 @@
 const faker = require('faker');
 
+/*
+// Faker-Data Profile Generator
+
 const profiles = [...new Array(5)].map((i, idx) => ({
   id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
   avatarUrl: faker.image.avatar(),
@@ -9,6 +12,17 @@ const profiles = [...new Array(5)].map((i, idx) => ({
       ? 'Test001 User'
       : `${faker.name.firstName()} ${faker.name.lastName()}`,
   is_admin: idx === 0 ? 1 : 0,
+})); 
+*/
+
+// Only 8 Okta-approved profiles
+// ID 1 and 2 are admins, ID 3-8 are users
+const profiles = [...new Array(8)].map((i, idx) => ({
+  id: idx + 1,
+  email: `llama00${idx + 1}@maildrop.cc`,
+  name: idx < 2 ? `Admin${idx + 1}` : `User${idx + 1}`,
+  is_admin: idx < 2 ? 1 : 0,
+  avatarUrl: faker.image.avatar(),
 }));
 
 exports.seed = function (knex) {
