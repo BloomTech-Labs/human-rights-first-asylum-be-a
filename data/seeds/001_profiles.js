9ca69e9ee3e5dade5ceaa5790d2eb79672f90b0f
Trevor Beadle <trevor.beadle.1996@gmail.com>
2021-01-28 14:48:14 -0600

    updated profile seeds with the correct okta-ids

--- data/seeds/001_profiles.js
+++ data/seeds/001_profiles.js
@@ -1,20 +1,5 @@
 const faker = require('faker');
 
-/*
-// Faker-Data Profile Generator
-
-const profiles = [...new Array(5)].map((i, idx) => ({
-  id: idx === 0 ? '00ulthapbErVUwVJy4x6' : faker.random.alphaNumeric(20),
-  avatarUrl: faker.image.avatar(),
-  email: idx === 0 ? 'llama001@maildrop.cc"' : faker.internet.email(),
-  name:
-    idx === 0
-      ? 'Test001 User'
-      : `${faker.name.firstName()} ${faker.name.lastName()}`,
-  is_admin: idx === 0 ? 1 : 0,
-})); 
-*/
-
 // Only 8 Okta-approved profiles
 // ID 1 and 2 are admins, ID 3-8 are users
 const profiles = [...new Array(8)].map((i, idx) => ({
@@ -24,6 +9,21 @@ const profiles = [...new Array(8)].map((i, idx) => ({
   is_admin: idx < 2 ? 1 : 0,
   avatarUrl: faker.image.avatar(),
 }));
+
+/*
+  Manually setting the `id` for each profile to the Okta provided ID. Adding
+  profiles was not in scope for this iteration, but adding profiles in the 
+  future will require the okta-id to be set as the `id` for each profile.
+*/
+
+profiles[0].id = '00ulthapbErVUwVJy4x6';
+profiles[1].id = '00ultwew80Onb2vOT4x6';
+profiles[2].id = '00ultx74kMUmEW8054x6';
+profiles[3].id = '00ultwqjtqt4VCcS24x6';
+profiles[4].id = '00ultwz1n9ORpNFc04x6';
+profiles[5].id = '00u13omswyZM1xVya4x7';
+profiles[6].id = '00u13ol5x1kmKxVJU4x7';
+profiles[7].id = '00u13oned0U8XP8Mb4x7';
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
