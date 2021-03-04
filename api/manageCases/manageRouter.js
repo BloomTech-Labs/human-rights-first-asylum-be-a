47e7373e71501cb21d48dfdff1aae9cecc0be81e
Sebastian Mendoza <sebass42.sebastianmendoza482@gmail.com>
2021-03-04 15:25:54 -0700

    Moved manage operations to new folder and changed operation routes

---
+++ api/manageCases/manageRouter.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const Cases = require('./manageModel');
+const router = express.Router();
+
+/**
+ * @swagger
+ * /manage/all:
+ *  get:
+ *    description: Finds all approved cases
+ *    summary: Returns all approved cases
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - cases
+ *    responses:
+ *      200:
+ *        description: An approved cases object
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Case'
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      404:
+ *        description: 'approved cases not found'
+ */
+router.get('/all', (req, res) => {
+  // const key = String(req.originalUrl);
+  Cases.findAllApproved()
+    .then((cases) => {
+      // Cache.makeCache(JSON.stringify(cases));
+      res.status(200).json(cases);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+/**
+ * @swagger
+ * /manage/approve:
+ *  get:
+ *    description: Approves a case
+ *    summary: Returns approved case
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - cases
+ *    responses:
+ *      200:
+ *        description: An approved case object
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Case'
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      404:
+ *        description: 'approved case not found'
+ */
+router.post('/approve', (req, res) => {
+  Cases.approve()
+    .then((cases) => {
+      // Cache.makeCache(JSON.stringify(cases));
+      res.status(200).json(cases);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+/**
+ * @swagger
+ * /manage/reject:
+ *  get:
+ *    description: Rejects case
+ *    summary: Rejects case from entering db
+ *    security:
+ *      - okta: []
+ *    tags:
+ *      - cases
+ *    responses:
+ *      200:
+ *        description: rejected case object
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Case'
+ *      401:
+ *        $ref: '#/components/responses/UnauthorizedError'
+ *      404:
+ *        description: 'approved cases not found'
+ */
+router.delete('/reject', (req, res) => {
+  Cases.approve()
+    .then((cases) => {
+      // Cache.makeCache(JSON.stringify(cases));
+      res.status(200).json(cases);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err.message });
+    });
+});
+
+module.exports = router;
