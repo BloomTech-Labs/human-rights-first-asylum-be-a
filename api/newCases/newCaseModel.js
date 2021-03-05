0b2a2110caa2c3c6685248417203b31885ac18e8
Sahar Jafari <shr.jafari@gmail.com>
2021-03-04 18:09:21 -0800

    Add delete endpoint

--- api/newCases/newCaseModel.js
+++ api/newCases/newCaseModel.js
@@ -4,11 +4,26 @@ const getAll = () => {
   return db('unapproved_cases');
 };
 
+const getById = (id) => {
+  return db('unapproved_cases').where({ id });
+};
+
 const add = async (data) => {
   return await db('unapproved_cases').insert(data);
+};
+
+const approve = (data) => {
+  // add case to the approve-table
+  // then remove(data.id);
+};
+
+const remove = async (id) => {
+  return await db('unapproved_cases').where({ id: 'sjjjs' }).del();
 };
 
 module.exports = {
   add,
   getAll,
+  remove,
+  getById,
 };
