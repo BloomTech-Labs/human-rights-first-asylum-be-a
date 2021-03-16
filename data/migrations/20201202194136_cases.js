67896317658aa0288948f3ae3b71814d476a46a6
kristapants <peachykeen@gmail.com>
2021-03-16 15:07:06 -0500

    changed table column types for boolean type columns to string where appropriate based on the stand up meeting with Rebecca

--- data/migrations/20201202194136_cases.js
+++ data/migrations/20201202194136_cases.js
@@ -7,19 +7,20 @@ exports.up = function (knex) {
       table.boolean('initial_or_appellate');
       table.string('hearing_date');
       table.string('judge');
-      table.boolean('initial_or_appellate');
       table.string('case_origin');
       table.boolean('case_filed_within_one_year');
       table.string('application_type');
-      table.boolean('protected_ground');
+      table.string('protected_ground');
       table.string('case_outcome');
       table.string('nation_of_origin');
       table.string('applicant_sex'); // should this be gender identity
       table.string('type_of_violence_experienced');
       table.string('applicant_indigenous_group');
       table.string('applicant_language');
-      table.string('applicant_access_to_interpreter');
-      table.string('applicant_perceived_credibility');
+      table.boolean('applicant_access_to_interpreter');
+      table.boolean('applicant_perceived_credibility');
+      //table.string('case_status'); This field is not present pending a review by the stakeholder. Also present in Seeds, bookmark case, approved case and unapproved case tables
+      //table.string('hearing_type'); This field is not present pending a review by the stakeholder. Also present in Seeds, bookmark case, approved case and unapproved case tables
     });
 };
 
