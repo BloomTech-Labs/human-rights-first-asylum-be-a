f05e538d4eeefbef4a3647f702be75b66674ddb7
aydinsenih <aydinsenih@gmail.com>
2021-04-12 14:00:01 -0700

    case approval needs more information about the case. to be able to approve a case judgeID, protected_ground, case_outcome, and hearing_date are required

--- api/newCases/newCaseModel.js
+++ api/newCases/newCaseModel.js
@@ -12,17 +12,23 @@ const add = async (data) => {
   return await db('unapproved_cases').insert(data);
 };
 
-const approve = async (id, judgeId) => {
-  //protected_ground , case_outcome, hearing_date will be added ...
-  const approvedCase = await db('unapproved_cases').where({ primary_key: id });
-  approvedCase[0]['judge'] = judgeId;
+const approve = async (
+  id,
+  judgeId,
+  protected_ground,
+  case_outcome,
+  hearing_date
+) => {
+  const approvedCases = await db('unapproved_cases').where({ primary_key: id });
+  const approvedCase = approvedCases[0];
+  approvedCase['judge'] = judgeId;
+  approvedCase['protected_ground'] = protected_ground;
+  approvedCase['case_outcome'] = case_outcome;
+  approvedCase['hearing_date'] = hearing_date;
   await db('cases').insert(approvedCase);
   await db('unapproved_cases').where({ primary_key: id }).del();
   console.log(await db('cases').where({ primary_key: id }));
   return await db('cases').where({ primary_key: id });
-  // const = await db('cases').insert()
-  // add case to the approve-table
-  // then remove(data.id);
 };
 
 const remove = async (id) => {
