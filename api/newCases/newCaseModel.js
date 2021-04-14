84b32e5d039fd52f092bc394ef4a6c608de25205
aydinsenih <aydinsenih@gmail.com>
2021-04-14 10:13:08 -0700

    judge info added to unapproved_cases seed. approving a case requires caseId only

--- api/newCases/newCaseModel.js
+++ api/newCases/newCaseModel.js
@@ -13,19 +13,9 @@ const add = async (data) => {
   return await db('unapproved_cases').insert(data);
 };
 
-const approve = async (
-  id,
-  judgeId,
-  protected_ground, //true or false
-  case_outcome, // Denied / Remanded / Granted / Sustained / Terminated
-  hearing_date // m-DD-YYYY
-) => {
+const approve = async (id) => {
   const approvedCases = await db('unapproved_cases').where({ primary_key: id });
   const approvedCase = approvedCases[0];
-  approvedCase['judge'] = judgeId;
-  approvedCase['protected_ground'] = protected_ground;
-  approvedCase['case_outcome'] = case_outcome;
-  approvedCase['hearing_date'] = hearing_date;
   approvedCase['primary_key'] = uuidv4();
   await db('cases').insert(approvedCase);
   await db('unapproved_cases').where({ primary_key: id }).del();
