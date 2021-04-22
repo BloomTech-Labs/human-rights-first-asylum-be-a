97935f9f3b63d7341fffa8388e6cb4cb75584d04
Matthew Justice <matthewajustice@gmail.com>
2021-04-21 20:24:34 -0400

    adds proper error handling, removes console logs and commented out code

--- api/upload/uploadRouter.js
+++ api/upload/uploadRouter.js
@@ -6,25 +6,12 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 const AWS = require('aws-sdk');
 const router = express.Router();
-// const db = require('../cases/caseModel');
 require('dotenv').config();
 
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
 });
-
-// body: {
-//     application: '',
-//     date: '2000-12-31',
-//     country_of_origin: 'Tunisia',
-//     panel_members: '',
-//     outcome: 'grant',
-//     protected_grounds: 'nationality',
-//     based_violence: '',
-//     references: '',
-//     sex_of_applicant: 'male'
-//   }
 
 const uploadFile = (fileName) => {
   const fileContent = fs.readFileSync(
@@ -40,11 +27,10 @@ const uploadFile = (fileName) => {
 
   return s3Upload
     .then((res) => {
-      console.log('Success');
       return res;
     })
-    .catch(() => {
-      console.log('Failure');
+    .catch((err) => {
+      return err;
     });
 };
 
@@ -65,41 +51,41 @@ router.post('/', async (req, res) => {
     return res.status(400).send('No files were uploaded.');
   }
 
-  // Accessing the file by the <input> File name="target_file"
   let targetFile = req.files.target_file;
   let leUUID = uuidv4();
 
-  //mv(path, CB function(err))
   targetFile.mv(path.join(__dirname, 'uploads', `${leUUID}.pdf`), (err) => {
     if (err) return res.status(500).send(err);
-    uploadFile(leUUID).then(() => {
-      console.log('Upload Successful');
-
-      axios
-        .post(`${process.env.DS_API_URL}${leUUID}`, { name: leUUID })
-        .then((scrape) => {
-          console.log(scrape.data);
-          const result = scrape.data.body;
-          const newCase = {
-            date: result.date,
-            judge: '',
-            case_outcome: result.outcome,
-            country_of_origin: result['country of origin'],
-            protegted_grounds: result['protected grounds'],
-            application_type: '',
-            case_outcome_city: '',
-            case_outcome_state: '',
-            gender: result.gender,
-            applicant_language: result['applicant language'],
-            indigenous_group: result.indigenous,
-            type_of_violence: '',
-            initial_or_appelate: '',
-            filed_in_one_year: result['check for one year'],
-            credible: '',
-          };
-          return res.status(200).json({ stuff: newCase });
-        });
-    });
+    uploadFile(leUUID)
+      .then(() => {
+        axios
+          .post(`${process.env.DS_API_URL}${leUUID}`, { name: leUUID })
+          .then((scrape) => {
+            const result = scrape.data.body;
+            //Any newCase value that is an empty string should be considered a work in progress of the scraper
+            const newCase = {
+              date: result.date,
+              judge: '',
+              case_outcome: result.outcome,
+              country_of_origin: result['country of origin'],
+              protegted_grounds: result['protected grounds'],
+              application_type: '',
+              case_outcome_city: '',
+              case_outcome_state: '',
+              gender: '',
+              applicant_language: '',
+              indigenous_group: '',
+              type_of_violence: '',
+              initial_or_appelate: '',
+              filed_in_one_year: '',
+              credible: '',
+            };
+            return res.status(200).json({ stuff: newCase });
+          });
+      })
+      .catch((err) => {
+        return res.status(500).json({ error: err });
+      });
   });
 });
 module.exports = router;
