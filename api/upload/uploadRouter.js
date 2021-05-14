e02661f0d8b725a517e418e38ce1a75cc4e95e51
WeiWu2 <weidiwu207@gmail.com>
2021-05-14 15:06:36 -0400

    adds new pending_cases table to db

--- api/upload/uploadRouter.js
+++ api/upload/uploadRouter.js
@@ -7,6 +7,7 @@ const fs = require('fs');
 const AWS = require('aws-sdk');
 const router = express.Router();
 require('dotenv').config();
+const uploadModel = require('./uploadModel');
 const authRequired = require('../middleware/authRequired');
 
 const s3 = new AWS.S3({
@@ -42,11 +43,6 @@ router.use(
     tempFileDir: path.join(__dirname, 'tmp'),
   })
 );
-
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 router.post('/', authRequired, (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     console.log(req);
@@ -70,19 +66,18 @@ router.post('/', authRequired, (req, res) => {
           .then((scrape) => {
             const result = scrape.data.body;
             // Any newCase value that doesn't reference the result should be considered a work in progress of the scraper and will need to be updated as the scraper grows
-
-            // this should be for FE to deal with, just send result and/or s3return for location?
-            // This does not take into account that result might send back null values which breaks the FE
-            // these values should be defaulted to '' if result values are null
+            console.log(result);
+            console.log(req.profile.id);
             const newCase = {
-              case_id: UUID,
+              pending_case_id: UUID,
+              user_id: req.profile.id,
               case_url: s3return.Location,
               case_number: '',
-              date: result.date,
+              date: result.date || '',
               judge: '',
-              case_outcome: result.outcome,
-              country_of_origin: result['country of origin'],
-              protected_grounds: result['protected grounds'],
+              case_outcome: result.outcome || '',
+              country_of_origin: result['country of origin'] || '',
+              protected_grounds: result['protected grounds'] || '',
               application_type: '',
               case_origin_city: '',
               case_origin_state: '',
@@ -93,8 +88,10 @@ router.post('/', authRequired, (req, res) => {
               appellate: false,
               filed_in_one_year: false,
               credible: false,
+              status: 'pending',
             };
-            return res.status(200).json(newCase);
+            uploadModel.add(newCase);
+            return res.status(200).json({});
           });
       })
       .catch((err) => {
---
+++ data/migrations/20210514135332_pending_cases.js
@@ -0,0 +1,35 @@
+exports.up = function (knex) {
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('pending_cases', function (table) {
+      table.string('pending_case_id').notNullable().primary();
+      table
+        .string('user_id')
+        .references('id')
+        .inTable('profiles')
+        .onDelete('RESTRICT');
+      table.string('case_url');
+      table.string('case_number');
+      table.string('date');
+      table.string('judge');
+      table.string('case_outcome');
+      table.string('country_of_origin');
+      table.string('protected_grounds');
+      table.string('application_type');
+      table.string('case_origin_city');
+      table.string('case_origin_state');
+      table.string('gender');
+      table.string('applicant_language');
+      table.string('indigenous_group');
+      table.string('type_of_violence');
+      table.boolean('appellate');
+      table.boolean('filed_in_one_year');
+      table.boolean('credible');
+      table.string('status');
+      table.string('uploaded');
+    });
+};
+
+exports.down = (knex) => {
+  return knex.schema.dropTableIfExists('pending_cases');
+};
