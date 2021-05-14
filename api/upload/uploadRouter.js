5f92ca7a42e3814de264fe9b7bd28080b2d524d8
WeiWu2 <weidiwu207@gmail.com>
2021-05-14 18:23:10 -0400

    updates old env name to new env

--- api/upload/uploadRouter.js
+++ api/upload/uploadRouter.js
@@ -11,8 +11,8 @@ const uploadModel = require('./uploadModel');
 const authRequired = require('../middleware/authRequired');
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
 const uploadFile = (fileName) => {
