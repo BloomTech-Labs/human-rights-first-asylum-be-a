b1e399cc2db4ffcef4dcdd44ceb27d8138f24ac3
Michelle Chen <mchen0062@gmail.com>
2021-05-16 11:57:56 -0700

    add changStatus function to update progress of cases

--- config/knexfile.js
+++ config/knexfile.js
@@ -4,7 +4,7 @@ dotenv.config({ path: '../.env' });
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DEV_DATABASE_URL,
     migrations: { directory: '../data/migrations' },
     seeds: { directory: '../data/seeds' },
     pool: {
