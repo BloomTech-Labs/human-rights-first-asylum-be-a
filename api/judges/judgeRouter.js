1826bbbcb064929a8d3806e4e525e65a1044bd75
Daniel Gamboa <danielgamboa10@gmail.com>
2021-05-20 15:31:15 -0500

    Update GET endpoint to issue POST request to DS API -- pending DS API endpoint

--- api/judges/judgeRouter.js
+++ api/judges/judgeRouter.js
@@ -37,20 +37,20 @@ router.get('/:judge_id/cases', async (req, res, next) => {
   // Grab the judge cases data
   try {
     const raw_data = await Judges.findJudgeCases(req.params.judge_id);
-    res.status(200).json({ data: raw_data });
+    // Issue POST req to DS API
+    axios
+      .post(`${process.env.DS_API_URL}${/*from DS team*/}`, { data: raw_data }) // What endpoint to hit?
+      .then((data_viz_res) => {
+        res.status(200).json({ data: data_viz_res.data }); // What is the data structure that comes back from DS?
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err });
+      });
+
   } catch (err) {
     res.status(404).json({ err });
   }
 
-  // Issue POST req to DS API
-  // axios
-  //   .post(`${process.env.DS_API_URL}${/*from DS team*/}`, { data: raw_data }) // What endpoint to hit?
-  //   .then((data_viz_res) => {
-  //     res.status(200).json({ data: data_viz_res.data }); // What is the data structure that comes back from DS?
-  //   })
-  //   .catch((err) => {
-  //     res.status(500).json({ error: err });
-  //   });
 });
 
 /**
