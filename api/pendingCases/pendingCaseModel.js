a7e4d3d8c9df01f3a9b278d77130731920704c62
WeiWu2 <weidiwu207@gmail.com>
2021-05-22 23:41:23 -0400

    new endpoint for admin review case

--- api/pendingCases/pendingCaseModel.js
+++ api/pendingCases/pendingCaseModel.js
@@ -5,32 +5,32 @@ const getAll = () => {
   return db('pending_cases');
 };
 
-const getById = (user_id) => {
+const getByUserId = (user_id) => {
   return db('pending_cases').where({ user_id });
 };
-
+const getByCaseId = (pending_case_id) => {
+  return db('pending_cases').where({ pending_case_id }).first();
+};
 const add = async (data) => {
   return await db('pending_cases').insert(data);
 };
-
-// reference for future
-// const approve = async (id) => {
-//   const approvedCases = await db('pending_cases').where({ primary_key: id });
-//   const approvedCase = approvedCases[0];
-//   approvedCase['primary_key'] = uuidv4();
-//   await db('cases').insert(approvedCase);
-//   await db('pending_cases').where({ primary_key: id }).del();
-//   return await db('cases').where({ primary_key: id });
-// };
-
+const update = async (pending_case_id, data) => {
+  return await db('pending_cases').where({ pending_case_id }).update(data);
+};
 const remove = async (pending_case_id) => {
   return await db('pending_cases').where({ pending_case_id }).del();
 };
-
+const changeStatus = async (id, newStatus) => {
+  return await db('pending_cases')
+    .where({ pending_case_id: id })
+    .update({ status: newStatus });
+};
 module.exports = {
   add,
   getAll,
   remove,
-  getById,
-  // approve,
+  getByUserId,
+  update,
+  changeStatus,
+  getByCaseId,
 };
