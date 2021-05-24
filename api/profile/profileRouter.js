b2cb6075c33d7366b382e645a28fc805c5cf2be9
Michelle Chen <68928523+MichelleCCodes@users.noreply.github.com>
2021-05-24 11:34:15 -0700

    formatting

--- api/profile/profileRouter.js
+++ api/profile/profileRouter.js
@@ -176,10 +176,8 @@ router.delete('/:id', authRequired, (req, res) => {
         })
         .catch(() => res.json({ message: 'Okta failed to delete this user' }));
       Profiles.remove(id).then((profiles) => {
-        res
-          .status(200)
-          .json({ 
-          message: `Profile '${id}' was deleted.`, 
+        res.status(200).json({ 
+          message: `Profile '${id}' was deleted.`,
           profiles: profiles,
         });
       });
