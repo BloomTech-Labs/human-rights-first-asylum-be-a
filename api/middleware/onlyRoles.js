432e701f2e86929c7d73aa0913a2cb202f011d90
Rebecca Peckman <rebeccapeckman@gmail.com>
2021-07-16 18:43:57 -0400

    updated doc comment

--- api/middleware/onlyRoles.js
+++ api/middleware/onlyRoles.js
@@ -2,7 +2,8 @@
  * Accepts an array of role_ids which have permission to access the endpoint,
  * if the role_id in the profile added to the request header by authRequired
  * middleware matches one of the role_ids in the parameter array, proceeds with
- * the request, otherwise sends a 403 response and denies access.
+ * the request, otherwise sends a 403 response and denies access. It also allows
+ * any user to read or update their own data.
  */
 const onlyRoles = (role_ids) => (req, res, next) => {
   const profile = req.profile;
