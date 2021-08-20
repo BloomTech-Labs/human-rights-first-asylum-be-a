ec22f530893cd97a95a2f0fbb35339794593a6b3
Hugo-Orozco <aceasin@outlook.com>
2021-08-20 14:18:34 -0600

    Notification Seed Data

---
+++ data/seeds/008-notif_types.js
@@ -0,0 +1,30 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('notif_types')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('notif_types').insert([
+        {
+          notif_id: 0,
+          notif_name: 'New User Registration',
+          notif_description: 'A new user has requested to join the application',
+        },
+        {
+          notif_id: 2,
+          notif_name: 'Judge Report',
+          notif_description: 'A judge you follow has handed down a decision',
+        },
+        {
+          notif_id: 2,
+          notif_name: 'Case Uploaded',
+          notif_description: 'Successfully uploaded case : #example',
+        },
+        {
+          notif_id: 3,
+          notif_name: 'New FAQ added',
+          notif_description: 'Another admin has addded a FAQ',
+        },
+      ]);
+    });
+};
